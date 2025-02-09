d5795500ef309a1182c7ad0c27e2dcbcf9911b2a
Raghav Mathur <raghav.b.mathur@avanade.com>
2025-02-09 11:18:38 -0700

    manual commit

--- src/main/java/com/example/Kafkaconsumer/service/TopicListener.java
+++ src/main/java/com/example/Kafkaconsumer/service/TopicListener.java
@@ -4,7 +4,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.kafka.listener.MessageListenerContainer;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -19,13 +25,13 @@ public class TopicListener {
     private String groupID;
 
     @KafkaListener(topics = "${topic.name.consumer}", groupId = "${spring.kafka.consumer.group-id}")
-    public void consume(ConsumerRecord<String, String> payload) throws InterruptedException {
+    public void consume(ConsumerRecord<String, String> payload, Acknowledgment acknowledgment) throws InterruptedException {
 
         String logMessage = String.format( "tópic: %1$s --- partition : %2$s --- message : %3$s --- offset : %4$s" ,
                 payload.topic(), payload.partition(), payload.value(), payload.offset());
-        Thread.sleep(1000);
-        log.info(logMessage);
+        Thread.sleep(400);
+        acknowledgment.acknowledge();
+        log.info(logMessage + " committed");
 
     }
-
 }
